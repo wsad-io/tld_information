086a8350344fdb9e516943bf45ffd43c6390287d
Ivan Malone <ivan.m@wsad.io>
2020-03-01 06:41:41 +0000

    public

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
         {
         node 
             {
-            label 'sunfire'
+            label 'zerozero'
             }
         }
       stages
--- process-mongo.bash
+++ process-mongo.bash
@@ -4,8 +4,8 @@
 function processZonefiles()
 	{
 	cd zonefiles/zonefiles
-        HOST=192.168.86.27
-        PORT=27017
+        HOST=54.188.121.97
+        PORT=27018
         TYPE=tsv
         COLLECTION=tld_zonedata
         DB=publicDB
